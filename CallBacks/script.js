7c90cbbdb8d209b9f012fe85fd05a579d8163eb8
Angelo Nicolson <angelonicolson@outlook.com>
2022-04-06 15:13:34 +1200

    Adding new folders. loops and operators for reference and practice

--- CallBacks/script.js
+++ CallBacks/script.js
@@ -1,18 +1,48 @@
-<button class="event-button">Callback button</button>
+const highlightEL = (myParam) => {
+    myParam = "hello";
+}
+console.log(myParam);
 
-    const button = document.querySector('.event-button');
-    button.addEventListener('click',function () {alert("Hey there!");
-});
 
-// Simple synchronous Callback
-function greeting(name) {
-    alert('Hello ' + name);
-  }
+
+// // Simple synchronous Callback
+// const greeting(name) {
+//     alert('Hello ' + name);
+//   }
   
-  function processUserInput(callback) {
-    var name = prompt('Please enter your name.');
-    callback(name);
-  }
+//   const processUserInput(callback) {
+//     var name = prompt('Please enter your name.');
+//     callback(name);
+//   }
   
-  processUserInput(greeting);
+//   processUserInput(greeting);
   
+// // this is anothe examle of a Callback
+//   const fullName(firstName, lastName, callback){
+//     console.log("My name is " + firstName + " " + lastName);
+//     callback(lastName);
+//   }
+  
+//   let greetings = function(ln){
+//     console.log('Welcome Mr. ' + ln);
+//   };
+  
+//   fullName("Jackie", "Chan", greeting);
+
+//   //Anonymous function
+//   setTimeout(function() {  
+//     console.log("This message is shown after 3 seconds");
+// }, 3000);
+
+// //callback as an arrow function
+// setTimeout(() => { 
+//     console.log("This message is shown after 3 seconds");
+// }, 3000);
+
+// // Callback button
+//  <button id="callback-btn">Click here</button>
+
+//  document.queryselector("#callback-btn")
+//    .addEventListener("click", function() {    
+//      console.log("User has clicked on the button!");
+// });
--- Event handlers/script.js
+++ Event handlers/script.js
@@ -12,11 +12,5 @@ const myDisplayer = (something) => {
   
   myCalculator(8, 16, myDisplayer);
 
-  // Event 
-  <button id="callback-btn">Click here</button>
-
-  document.queryselector("#callback-btn")
-    .addEventListener("click", function() {    
-      console.log("User has clicked on the button!");
-});
+ 
   
